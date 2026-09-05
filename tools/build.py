13065e28dd82dc6fca6e2c8c9e62ca7df1309e56
EmeraldIngot64 <75592635+EmeraldIngot64@users.noreply.github.com>
2026-09-05 11:01:16 -0400

    Add build script

---
+++ tools/build.py
@@ -0,0 +1,15 @@
+import zipfile
+from pathlib import Path
+
+output_path = Path("../build/trueclassic.zip")
+sources = ["../assets", "../style_164", "../pack.mcmeta", "../pack.png"]
+
+with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
+    for source in sources:
+        source = Path(source).resolve()
+        if (source.is_file()):
+            zip_file.write(source, arcname=source.name)
+        else:
+            for file in source.rglob("*"):
+                if (file.is_file()):
+                    zip_file.write(file, file.relative_to(source.parent))
